a20f5811902953c1e815f644492d5bf6a6c02358
drubim <drubimdrubim@gmail.com>
2025-12-21 01:18:15 -0300

    Delete public directory

--- public/indicador.js
+++
@@ -1,9 +0,0 @@
-fetch('/api/indicador/clientes', {
-  headers: { Authorization: 'Bearer ' + localStorage.token }
-})
-.then(r => r.json())
-.then(d => {
-  d.forEach(c => {
-    lista.innerHTML += `<li>${c.nome} - ${c.status_externo} - ${c.valor_carta || ''}</li>`;
-  });
-});
